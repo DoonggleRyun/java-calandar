56d7430bdb217a07ded73eda4f37e545c0b6bc55
seryun <devseryun@gmail.com>
2021-11-19 13:15:42 +0900

    Init Calendar project

---
+++ src/ryoomsae/calandar/Calandar.java
@@ -0,0 +1,11 @@
+package ryoomsae.calandar;
+
+public class Calandar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello, Calendar");
+
+	}
+
+}
